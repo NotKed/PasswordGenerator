6109564e0496ea2867a7394bed6773ff1b5a8206
NotKed <kyleedwards031003@gmail.com>
2021-01-02 03:46:56 +0000

    Initial version

---
+++ PasswordGenerator/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  PasswordGenerator
+//
+//  Created by Kyle Edwards on 01/01/2021.
+//
+
+import SwiftUI
+import MobileCoreServices
+
+struct ContentView: View {
+    
+    @State var pass: String = "Password Generator"
+    @State var copyVisible: Bool = false
+    
+    @State var chars: Array = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`! @#$%^&*()_-+={[}]|\"'<,>.?/")
+    
+    var body: some View {
+        VStack() {
+            HStack() {
+                Text(pass)
+                    .font(.title)
+                    .padding()
+                if copyVisible {
+                    Button(action: {
+                        UIPasteboard.general.string = pass
+                    }, label: {
+                        Image(systemName: "doc.on.doc.fill")
+                    }).buttonStyle(PlainButtonStyle())
+                }
+            }
+            Button(action: {
+                copyVisible = true
+                pass = "";
+                for _ in 1...35 {
+                    pass += String(chars.randomElement()!)
+                }
+            }, label: {
+                HStack() {
+                    Image(systemName: "gear")
+                        .font(.title)
+                    Text("Generate")
+                        .font(.title)
+                }
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Kyle Edwards on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
